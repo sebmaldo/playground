ad9caacd97310e8a6fd15975c593bef6ced00a43
Sebastián Maldonado <sebmaldo@gmail.com>
2017-10-12 12:17:17 -0300

    Add parcial logic to form buttom

--- react/1. React Getting Started/module-3/src/Form.jsx
+++ react/1. React Getting Started/module-3/src/Form.jsx
@@ -1,16 +1,32 @@
 import React, {Component} from 'react';
 
 class Form extends Component {
+    state = {
+        userName: ''
+    }
 
     handleSubmit = (event) => {
         event.preventDefault();
+        console.log('Event from submit: ', this.state.userName);
+        this.setState(()=>{
+            return {userName:''}
+        });
 
+    }
+
+    handleChange = (event) => {
+        this.setState(
+                 {userName: event.target.value}
+        );
     }
 
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <input type="text" placeholder="Github Username" required/>
+                <input type="text"
+                    value={this.state.userName}
+                    onChange={this.handleChange}
+                    placeholder="Github Username" required/>
                 <button type="submit">Add card</button>
             </form>
         )
