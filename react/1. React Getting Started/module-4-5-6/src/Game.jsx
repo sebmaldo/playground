0f826767e26b5d25f5c1b82f532a8879c2535168
Sebastián Maldonado <sebmaldo@gmail.com>
2017-10-14 21:42:14 -0300

    Add material layout to example

--- react/1. React Getting Started/module-4-5-6/src/Game.jsx
+++ react/1. React Getting Started/module-4-5-6/src/Game.jsx
@@ -2,19 +2,19 @@ import React, {Component} from 'react';
 import Stars from './Stars';
 import Button from './Button';
 import Answer from './Answer';
-import 'react-bootstrap';
+import Grid from 'material-ui/Grid'
 
 class Game extends Component {
 
     render(){
         return (
             <div>
-                <h3>Play Nine</h3>
-                <div className="row">
+                <h1>Play Nine</h1>
+                <Grid container direction='row' align='center' justify='space-around'>
                     <Stars />
                     <Button />
                     <Answer />
-                </div>
+                </Grid>
 
             </div>
         )
